763d0f8132411724024dab682da3e473b6cd6a3b
Amey Ghadigaonkar <gamoholic010@gmail.com>
2014-08-18 19:28:16 -0400

    first commit

---
+++ ReverseInteger.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+public class ReverseInteger {
+    public int reverse(int x) {
+        boolean negNumber = false;
+        if(x<0){
+            negNumber = true;
+            x *= -1;
+        }
+        int answer = 0;
+        char[] c = String.valueOf(x).toCharArray();
+        for(int i=c.length-1; i>=0; i--){
+            int temp = Character.getNumericValue(c[i]);
+            answer = answer*10 + temp;
+        }
+        
+        if(negNumber)
+            return -1*answer;
+        return answer;
+    }
+}
