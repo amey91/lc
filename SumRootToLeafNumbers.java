03e39e516c0c76b3c8d443403d0e3d2d1bf40f0a
Amey Ghadigaonkar <gamoholic010@gmail.com>
2014-09-28 19:54:45 -0400

    completed sum root to leaf numbers

---
+++ SumRootToLeafNumbers.java
@@ -0,0 +1,36 @@
+package leetcode;
+
+import leetcode.commons.TreeNode;
+
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+ /* my earlier solution used to fork strings at each non-leaf node
+  * this is better because recursion case for leaf is different and returns sum
+  * non-leafs return sum of leafs beneath them
+  */
+ 
+public class SumRootToLeafNumbers {
+    public int sumNumbers(TreeNode root) {
+        return parseTreeForSum(root, 0);
+    }
+    
+    private int parseTreeForSum(TreeNode root, int sumTillNow){
+        if(root==null)
+            return 0;
+        sumTillNow = sumTillNow*10 + root.val;
+        // if leaf node, return its sum
+        if(root.left==null && root.right==null)
+            return sumTillNow;
+        // else return sum of leaf nodes beneath
+        int leftSum = parseTreeForSum(root.left, sumTillNow);
+        int rightSum = parseTreeForSum(root.right, sumTillNow);
+        return leftSum+rightSum;
+    }
+}
