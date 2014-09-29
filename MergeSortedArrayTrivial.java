04b711d73ea104346938ad63eb1e48872edb9dda
Amey Ghadigaonkar <gamoholic010@gmail.com>
2014-09-28 20:18:42 -0400

    completed merge sorted array

---
+++ MergeSortedArrayTrivial.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+import java.util.Arrays;
+// takes nlogn time
+public class MergeSortedArrayTrivial {
+    public void merge(int A[], int m, int B[], int n) {
+        for(int i=m; i<m+n; i++){
+            A[i]=B[i-m];
+        }
+        Arrays.sort(A);
+    }
+}
