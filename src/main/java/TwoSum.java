f9bb520eb94e5dae5afeb4dfdf4fedee6d82dd65
Amey Ghadigaonkar <gamoholic010@gmail.com>
2016-01-10 18:31:26 -0800

    Update TwoSum

--- src/main/java/TwoSum.java
+++ src/main/java/TwoSum.java
@@ -1,21 +1,16 @@
 import java.util.HashMap;
+import java.util.Map;
 
 public class TwoSum {
-    public int[] twoSum(int[] numbers, int target) {
-        int[] result = new int[2];
-        HashMap<Integer,Integer> hm = new HashMap<>();
-        for(int i=0; i<numbers.length;i++){
-            if(hm.containsKey(target-numbers[i])){
-                int bigger;
-                int smaller;
-                result[0] = hm.get(target-numbers[i])+1;
-                result[1] = i+1;
-                return result;
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i= 0; i<nums.length; i++) {
+            if(map.containsKey(target - nums[i])) {
+                return new int[] {map.get(target-nums[i])+1, i+1};
             } else {
-                hm.put(numbers[i], i);
+                map.put(nums[i] , i);
             }
-            
         }
-        return result;
+        return null;
     }
 }
