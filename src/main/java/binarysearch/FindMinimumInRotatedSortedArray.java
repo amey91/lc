67ed87191841d382bff1b8bedccef41f2fd82a3a
Amey Ghadigaonkar <aghadiga@amazon.com>
2020-02-17 23:19:58 -0800

    Find minimum in sorted array

---
+++ src/main/java/binarysearch/FindMinimumInRotatedSortedArray.java
@@ -0,0 +1,17 @@
+package main.java.binarysearch;
+
+public class FindMinimumInRotatedSortedArray {
+    public int findMin(int[] nums) {
+        // the linear solution is faster than binary solutions for some reason
+        if (nums.length == 0) {
+            return 0;
+        }
+        int min = nums[0];
+        for (int i = 1; i < nums.length ; i++) {
+            if (min > nums[i]) {
+                return nums[i];
+            }
+        }
+        return min;
+    }
+}
