c284f8a0ee10e1099c70d32c38dce6b1dfd9670c
Amey Ghadigaonkar <aghadiga@amazon.com>
2020-04-15 00:47:32 -0700

    2 solutions

--- src/main/java/LongestPalindromicString.java
+++ src/main/java/LongestPalindromicString.java
@@ -1,64 +1,48 @@
+package main.java;
+
+// #5 https://leetcode.com/problems/longest-palindromic-substring/
 public class LongestPalindromicString {
-	
-   		static class Pair {
-		int left;
-		int right;
-		private Pair(String s, int left, int right){
-			this.left = left;
-			this.right = right;
-			correctIndex(s);
-		}
-		private void correctIndex(String s) {
-			if(left<0 || right>(s.length()-1)){
-				System.out.println("this code is shit");
-				left++;
-				right--;
+
+	class Solution {
+		public String longestPalindrome(String s) {
+			if (s== null || s.length() <= 1) {
+				return s;
 			}
+			char[] chars = s.toCharArray();
+			int length = 0;
+			int maxLeft = 0;
+			int maxRight = 0;
+			for (int i = 0; i<chars.length;i++) {
+				int[] indexes = findPalindrome(chars, i, i);
+				if (indexes!=null && length<indexes[1]-indexes[0]+1) {
+					length = indexes[1]-indexes[0]+1;
+					maxLeft=indexes[0];
+					maxRight=indexes[1];
+				}
+				if (i < chars.length - 1) {
+					indexes = findPalindrome(chars, i, i+1);
+					if (indexes!=null && length<indexes[1]-indexes[0]+1) {
+						length = indexes[1]-indexes[0]+1;
+						maxLeft=indexes[0];
+						maxRight=indexes[1];
+					}
+				}
+			}
+			return s.substring(maxLeft, maxRight+1);
 		}
-		
-	    private int length() {
-			return right-left+1;
-		}
-		public boolean greaterThan(Pair maxPalindrome) {
-			if(maxPalindrome==null)
-				return true;
-			if((right-left)>(maxPalindrome.right-maxPalindrome.left))
-				return true;
-			return false;
+
+		private int[] findPalindrome(char[] chars, int left, int right) {
+			while(left>=0 && right<chars.length && chars[left] == chars[right]) {
+				left--;
+				right++;
+			}
+			if (chars[left+1]==chars[right-1]) {
+				// palindrome found
+				// NOTE: I do lef++ and right-- because the indexes have already breached bounds when while loop ends
+				return new int[]{left+1, right-1};
+			}
+			// palindrome not found
+			return null;
 		}
 	}
-	
-    public String longestPalindrome(String s) {
-        if(s.equals("") || s==null)
-        	return s;
-    	Pair maxPalindrome = new Pair(s, 0, 0);
-        int maxLength = s.length(); 
-        for(int i=0; i<s.length();i++){
-            Pair result = getPalindrome(s,i);
-            if(result.length()==maxLength)
-                return s;
-            if(result.greaterThan(maxPalindrome)){
-                maxPalindrome = result;
-            }
-        }
-        return s.substring(maxPalindrome.left, maxPalindrome.right+1);
-    }
-    
-	private static Pair getPalindrome(String s, int pos){
-        Pair a = getPalindromeAtPosition(s, pos, pos);
-        Pair b= getPalindromeAtPosition(s, pos, pos+1);
-        if(Math.abs(a.left-a.right)>Math.abs(b.left-b.right)){
-        	return a;
-        } else {
-        	return b;
-        }
-    }
-    
-    private static Pair getPalindromeAtPosition(String s, int left, int right){
-        while(left>=0 && right<s.length() && String.valueOf(s.charAt(left)).equals(s.charAt(right)+"")){
-            left--;
-            right++;
-        }
-        return new Pair(s,left+1,right-1);
-    }
 }
--- src/main/java/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,25 +1,34 @@
+package main.java;
+
 import java.util.HashMap;
+import java.util.Map;
 
 public class LongestSubstringWithoutRepeatingCharacters {
-	public int lengthOfLongestSubstring(String s) {
-		if(s==null)
-			return 0;
-		char[] c = s.toCharArray();
-		int start = 0;
-		int count=0;
-		HashMap<Character, Integer> hm = new HashMap<>();
-		for(int i=0;i<c.length;i++){
-			if(!hm.containsKey(c[i])){
-                hm.put(c[i], i);
-                // was not getting accepted since I wasn't updating count in this case
-                count=Math.max(count, i-start+1);
-			} else {
-			    //check if another char has not repeated after the last occurence of current char
-                start=Math.max(start, hm.get(c[i])+1);
-                hm.put(c[i],i);
-                count=Math.max(count, i-start+1);
+	class Solution {
+		public int lengthOfLongestSubstring(String s) {
+
+			if (s == null) {
+				return 0;
 			}
+			if (s.length() <= 1) {
+				return s.length();
+			}
+
+			char[] charArr = s.toCharArray();
+
+			int startFromIndex = 0;
+			int length = 0;
+			// maps value to previously seen on index
+			Map<Character, Integer> map = new HashMap<>();
+			for (int i = 0; i<charArr.length; i++) { // i=2
+				if (map.containsKey(charArr[i])) {
+					int oldPreviousIndex = map.get(charArr[i]);
+					startFromIndex = Math.max(startFromIndex, oldPreviousIndex + 1); // 2
+				}
+				map.put(charArr[i], i); // a=0,b=2
+				length = Math.max(length, i - startFromIndex + 1); // 2
+			}
+			return length;
 		}
-		return Math.max(count,c.length-start);
 	}
 }
