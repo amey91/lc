b780767f7b51c3f8c553fce67e1e71327229a087
Amey Ghadigaonkar <aghadiga@amazon.com>
2020-04-25 20:16:53 -0700

    SortCharactersByFrequency

---
+++ src/main/java/SortCharactersByFrequency.java
@@ -0,0 +1,42 @@
+package main.java;
+
+// #451. Sort Characters By Frequency
+public class SortCharactersByFrequency {
+
+    class Solution {
+        public String frequencySort(String s) {
+            if (s == null || s.length() == 0) {
+                return s;
+            }
+
+            // all inputs fit within this array!!
+            int[] counts = new int[128];
+            char[] resultS = s.toCharArray();
+            for (char c: resultS) {
+
+                // I can directly map all chars to array indexes!
+                counts[c]++;
+            }
+            int currentIndex = 0;
+            while (currentIndex < resultS.length) {
+                int maxCount = 0;
+                int index = 0;
+
+                for (int i = 0; i < 128; i++) {
+                    if (counts[i] > maxCount) {
+                        maxCount = counts[i];
+                        index = i;
+                    }
+                }
+                while(maxCount > 0) {
+                    maxCount --;
+
+                    // Reuse input array because this is much faster than StringBuilder
+                    resultS[currentIndex++] = (char)index;
+                }
+                counts[index] = 0;
+            }
+            return new String(resultS);
+        }
+    }
+}
