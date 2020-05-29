87015a5c8b6ec01e40cc203bc9cb5fc6b3624f46
Amey Ghadigaonkar <aghadiga@amazon.com>
2020-05-28 23:06:53 -0700

    NestedListWeightSum2

--- src/main/javalang/NestedListWeightSum.java
+++ src/main/javalang/NestedListWeightSum.java
@@ -2,31 +2,11 @@ package javalang;
 
 import java.util.List;
 
+import javalang.commons.NestedInteger;
+
 public class NestedListWeightSum {
     // 339. Nested List Weight Sum
 // https://leetcode.com/problems/nested-list-weight-sum/
-
-    // This is the interface that allows for creating nested lists.
-    // You should not implement it, or speculate about its implementation
-    public interface NestedInteger {
-        // @return true if this NestedInteger holds a single integer, rather than a nested list.
-        public boolean isInteger();
-
-        // @return the single integer that this NestedInteger holds, if it holds a single integer
-        // Return null if this NestedInteger holds a nested list
-        public Integer getInteger();
-
-        // Set this NestedInteger to hold a single integer.
-        public void setInteger(int value);
-
-        // Set this NestedInteger to hold a nested list and adds a nested integer to it.
-        public void add(NestedInteger ni);
-
-        // @return the nested list that this NestedInteger holds, if it holds a nested list
-
-        // Return null if this NestedInteger holds a single integer
-        public List<NestedInteger> getList();
-    }
 
     class Solution {
         public int depthSum(List<NestedInteger> nestedList) {
