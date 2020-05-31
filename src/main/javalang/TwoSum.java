facc6b6986e1d4515692da2eec245d250b02d0eb
Amey Ghadigaonkar <aghadiga@amazon.com>
2020-05-31 16:04:44 -0700

    Revise TwoSum

--- src/main/javalang/TwoSum.java
+++ src/main/javalang/TwoSum.java
@@ -1,18 +1,26 @@
-package javalang;
+package javalang;// 1. Two Sum
+// https://leetcode.com/problems/two-sum/
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for(int i= 0; i<nums.length; i++) {
-            if(map.containsKey(target - nums[i])) {
-                return new int[] {map.get(target-nums[i])+1, i+1};
-            } else {
-                map.put(nums[i] , i);
-            }
+        int[] result = new int[2];
+        if (nums == null) {
+            return result;
         }
-        return null;
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                result[0] = map.get(target - nums[i]);
+                result[1] = i;
+                return result;
+            }
+            map.put(nums[i], i);
+        }
+        return result;
     }
 }
