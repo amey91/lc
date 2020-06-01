eeae82ef3efbf199e57255d62bf02bb50cda4ec6
Amey Ghadigaonkar <aghadiga@amazon.com>
2020-05-31 17:43:06 -0700

    PalindromePermutation

---
+++ src/main/javalang/PalindromePermutation.java
@@ -0,0 +1,37 @@
+package javalang;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class PalindromePermutation {
+    // 266. Palindrome Permutation
+// https://leetcode.com/problems/palindrome-permutation/
+    class Solution {
+
+        // n time
+        // n space
+        public boolean canPermutePalindrome(String s) {
+            if (s == null) {
+                return false;
+            }
+
+            if (s.length() < 2) {
+                return true;
+            }
+
+            char[] chars = s.toCharArray();
+
+            Set<Character> set = new HashSet<>();
+
+            for (char c : chars) {
+                if (set.contains(c)) {
+                    set.remove(c);
+                } else {
+                    set.add(c);
+                }
+            }
+
+            return set.size() <= 1;
+        }
+    }
+}
