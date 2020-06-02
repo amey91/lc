a38d645b49af9b5c680b7e64749aae78e864c896
Amey Ghadigaonkar <aghadiga@amazon.com>
2020-06-02 11:33:28 -0700

    TwoSumBSTs

---
+++ src/main/javalang/TwoSumBSTs.java
@@ -0,0 +1,52 @@
+package javalang;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javalang.commons.TreeNode;
+
+public class TwoSumBSTs {
+
+
+// 1. this stores all values in first tree and then iterates second tree with that information to see if we have a node in first tree that match nodes in the seconds one
+// time = N+M where N,M are number of nodes in both tree; space = N
+
+// 2. there is another solution where we iterate first tree and for every node in first, we try to find a node in the second one
+// time = N*logM; space = 2
+
+    class Solution {
+        public boolean twoSumBSTs(TreeNode root1, TreeNode root2, int target) {
+            if (root1 == null || root2 == null) {
+                return false;
+            }
+
+            Set<Integer> seen = new HashSet<>();
+            collectValues(root1, seen);
+
+            return iterate(root2, seen, target);
+
+        }
+
+        private boolean iterate(TreeNode node, Set<Integer> seen, int target) {
+            if (node == null) {
+                return false;
+            }
+
+            if (seen.contains(target - node.val)) {
+                return true;
+            }
+
+            return iterate(node.left, seen, target) || iterate(node.right, seen, target);
+        }
+
+        private void collectValues(TreeNode node, Set<Integer> seen) {
+            if (node == null) {
+                return;
+            }
+
+            seen.add(node.val);
+            collectValues(node.left, seen);
+            collectValues(node.right, seen);
+        }
+    }
+}
