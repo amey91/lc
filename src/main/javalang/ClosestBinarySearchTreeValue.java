e0678bb75ccf32b98407ff210ed6f46d3ce9f985
Amey Ghadigaonkar <aghadiga@amazon.com>
2020-06-11 22:58:09 -0700

    ClosestBinarySearchTreeValue revised!

--- src/main/javalang/ClosestBinarySearchTreeValue.java
+++ src/main/javalang/ClosestBinarySearchTreeValue.java
@@ -5,36 +5,54 @@ import javalang.commons.TreeNode;
 public class ClosestBinarySearchTreeValue {
     // 270. Closest Binary Search Tree Value
 // https://leetcode.com/problems/closest-binary-search-tree-value/
+
+    // find closest in bst by choosing correct branch. We can go to both branhces, but this is more efficient and gives logn time
     class Solution {
 
-        double minDiff = 0;
-        int minValue = 0;
-
+        // time = logn
+        // space = 1
         public int closestValue(TreeNode root, double target) {
-            if (root == null) {
-                return Integer.MAX_VALUE;
+            int answer = root.val;
+            while (root != null) {
+                if (Math.abs(root.val - target) < Math.abs(answer - target)) {
+                    answer = root.val;
+                }
+
+                if (root.val > target) {
+                    root = root.left;
+                } else {
+                    root = root.right;
+                }
             }
-            minDiff = Math.abs(root.val - target);
-            minValue = root.val;
-            findClosest(root.left, target);
-            findClosest(root.right, target);
-            return minValue;
+            return answer;
         }
 
-        private void findClosest(TreeNode root, double target) {
-            if (root == null) {
-                return;
-            }
-            if (Math.abs(root.val - target) <= minDiff) {
-                minValue = root.val;
-                minDiff = Math.abs(root.val - target);
-            }
-            if (target < root.val) {
-                findClosest(root.left, target);
-            } else {
-                findClosest(root.right, target);
-            }
-        }
+
+        // time = logn
+        // space = logn for recursion stack
+//     public int closestValue_recursive(TreeNode root, double target) {
+//         if (root.val == target) {
+//             return root.val;
+//         }
+
+//         int min = root.val;
+
+//         if (target  < root.val) {
+//             if (root.left != null) {
+//                 int left = closestValue(root.left, target);
+//                 if (Math.abs(left - target) < Math.abs(min - target)) {
+//                     min = left;
+//                 }
+//             }
+//         } else {
+//             if (root.right != null) {
+//                 int right = closestValue(root.right, target);
+//                 if (Math.abs(right - target) < Math.abs(min - target)) {
+//                     min = right;
+//                 }
+//             }
+//         }
+//         return min;
+//     }
     }
-
 }
