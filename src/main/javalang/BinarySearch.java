93470bce13af8af7439bf9b4025be0c1afe5bbbc
Amey Ghadigaonkar <aghadiga@amazon.com>
2020-06-14 17:22:04 -0700

    comment

--- src/main/javalang/BinarySearch.java
+++ src/main/javalang/BinarySearch.java
@@ -12,6 +12,10 @@ public class BinarySearch {
 
             int l = 0; int r = nums.length-1;
             int p;
+
+            // since we are not guaranteed for the target to be present, we need to let the pointers cross each other to know when to stop
+            // This can be done by always setting left and right to +/-1 of middle so that pointers cross. If we set the pointers to
+            // exactly middle then the pointers we never cross and loop to never end.
             while (l <= r) {
                 p = (l+r)/2;
                 if (nums[p] == target) {
